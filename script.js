97ea927347874cce122e15039ca9347f5800a525
Hiradabd <hiradabdolahpor810@gmail.com>
2024-12-18 14:05:32 +0330

    Update script.js

--- script.js
+++ script.js
@@ -2,28 +2,32 @@ document.addEventListener('DOMContentLoaded', () => {
     const falButton = document.getElementById('falButton');
     const falResult = document.getElementById('falResult');
     let timer;
-    let falHafez;
 
-    
-    fetch('falHafez.json')
-        .then(response => response.json())
-        .then(data => {
-            falHafez = data.falHafez;
-        })
-        .catch(error => console.error('Error loading JSON:', error));
+    const falHafez = [
+        "سخن عشق تو بی‌آنکه برآید به زبانم",
+        "در ازل پرتو حسنت ز تجلی دم زد",
+        "دوست می‌دارم من این نالیدن دلسوز را",
+        "به کوی عشق منه بی‌دلیل راه قدم",
+        "ای دل غلام شاه جهان باش و شاه باش"
+    ];
 
-    falButton.addEventListener('mousedown', () => {
+    function startTimer() {
         timer = setTimeout(() => {
             const randomFal = falHafez[Math.floor(Math.random() * falHafez.length)];
             falResult.textContent = randomFal;
         }, 5000);
-    });
+    }
 
-    falButton.addEventListener('mouseup', () => {
+    function stopTimer() {
         clearTimeout(timer);
-    });
+    }
 
-    falButton.addEventListener('mouseleave', () => {
-        clearTimeout(timer);
-    });
+    falButton.addEventListener('mousedown', startTimer);
+    falButton.addEventListener('mouseup', stopTimer);
+    falButton.addEventListener('mouseleave', stopTimer);
+
+    // رویدادهای لمسی برای سازگاری با موبایل
+    falButton.addEventListener('touchstart', startTimer);
+    falButton.addEventListener('touchend', stopTimer);
+    falButton.addEventListener('touchcancel', stopTimer);
 });
